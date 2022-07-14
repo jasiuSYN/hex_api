09d690134deeee1108818963b92fcb42cb7abfc1
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2022-07-15 00:01:37 +0200

    Register models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Tier, Image
 
 # Register your models here.
+admin.site.register(Tier)
+admin.site.register(User)
+admin.site.register(Image)
